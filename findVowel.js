51fa4e5b54b31051fb136038d21ac510e5253657
vanh2001 <anhviet1709@gmail.com>
2022-01-27 10:25:59 +0700

    Update file End Test

--- findVowel.js
+++ findVowel.js
@@ -1,9 +1,12 @@
-let arr = ['a', 'o', 'g', 'h', 'e'];
+let arr = [  'g', 'h'];
 function findVowel() {
     let count = 0
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === 'a' || arr[i] === 'o' || arr[i] === 'e' || arr[i] === 'u' || arr[i] === 'i'){
             count += 1;
+        }else {
+            alert('Không có nguyên âm nào')
+            break;
         }
     }
     alert('Tổng số nguyên âm trong mảng: ' + count);
